c3cb5b8534ce04b3996a4355acb96263c05ee256
Jordan TETE <tetej171@gmail.com>
2024-09-23 16:29:55 +0200

    refactor OrderManager to easy readability and future improvements - part 1

--- order_management/order_manager.py
+++ order_management/order_manager.py
@@ -7,26 +7,27 @@ class OrderManager:
     def __init__(self, config_manager):
         self.logger = logging.getLogger(self.__class__.__name__)
         self.config_manager = config_manager
-        self.trading_fee = self.config_manager.get_trading_fee() # trading_fee is in decimal (e.g., 0.001 for 0.1%)
+        self.trading_fee = self.config_manager.get_trading_fee()
         self.trade_percentage = 0.1 ## TODO: trade_percentage based on num_grids instead ? Default to 10%
         self.grid_levels = {}
-        self.buy_orders = []
-        self.sell_orders = []
         self.sorted_buy_grids = []
         self.sorted_sell_grids = []
         self.total_trading_fees = 0
 
     def initialize_grid_levels(self, grids, central_price):
-        self.grid_levels = {}
-
-        for price in grids:
-            if price <= central_price:
-                self.grid_levels[price] = GridLevel(price, GridCycleState.READY_TO_BUY)
-            else:
-                self.grid_levels[price] = GridLevel(price, GridCycleState.READY_TO_SELL)
-
+        self.grid_levels = {price: GridLevel(price, GridCycleState.READY_TO_BUY if price <= central_price else GridCycleState.READY_TO_SELL) for price in grids}
         self.sorted_buy_grids = [price for price in sorted(grids) if price <= central_price]
         self.sorted_sell_grids = [price for price in sorted(grids) if price > central_price]
+
+    ## TODO: is exceptions handled here ?
+    def execute_order(self, order_type: OrderType, current_price, previous_price, timestamp, balance, crypto_balance):
+        if order_type == OrderType.BUY:
+            if self.can_execute_buy_order(current_price, timestamp):
+                return self.execute_buy(current_price, previous_price, timestamp, balance, crypto_balance)
+        elif order_type == OrderType.SELL:
+            if self.can_execute_sell_order(current_price, timestamp):
+                return self.execute_sell(current_price, previous_price, timestamp, balance, crypto_balance)
+        raise ValueError(f"Cannot place {order_type} order at this time.")
     
     def can_execute_buy_order(self, current_price, timestamp):
         idx = bisect.bisect_right(self.sorted_buy_grids, current_price)
@@ -36,58 +37,67 @@ class OrderManager:
         idx = bisect.bisect_left(self.sorted_sell_grids, current_price)
         return idx > 0 and idx < len(self.sorted_sell_grids)
     
-    ## grid level crossed either upward or downward for both buy/sell check
     def detect_grid_level_crossing(self, current_price, previous_price, sell=False):
-        crossed_grid_level = None
-
-        if sell:
-            for grid_level in self.sorted_sell_grids:
-                if previous_price < grid_level and current_price >= grid_level:
-                    crossed_grid_level = self.grid_levels[grid_level]
-                    break
-        else:
-            for grid_level in self.sorted_buy_grids:
-                if previous_price > grid_level and current_price <= grid_level:
-                    crossed_grid_level = self.grid_levels[grid_level]
-                    break
-
-        return crossed_grid_level
+        grid_list = self.sorted_sell_grids if sell else self.sorted_buy_grids
+        for grid_price in grid_list:
+            if (sell and previous_price < grid_price <= current_price) or (not sell and previous_price > grid_price >= current_price):
+                return self.grid_levels[grid_price]
+        return None
 
     def execute_buy(self, current_price, previous_price, timestamp, balance, crypto_balance):
         try:
             grid_level_crossed = self.detect_grid_level_crossing(current_price, previous_price)
-
-            if grid_level_crossed is not None and grid_level_crossed.can_place_buy_order():
-                quantity = self.trade_percentage * balance / current_price
-                buy_order = Order(current_price, quantity, OrderType.BUY, timestamp)
-                grid_level_crossed.place_buy_order(buy_order)
-                trade_value = quantity * current_price
-                buy_fee = trade_value * self.trading_fee
-                balance -= trade_value + buy_fee
-                self.total_trading_fees += buy_fee
-                crypto_balance += quantity
-                self.buy_orders.append(buy_order)
-                return balance, crypto_balance
+            if self.is_valid_buy_level(grid_level_crossed):
+                return self.place_buy_order(grid_level_crossed, current_price, timestamp, balance, crypto_balance)
             else:
                 raise ValueError(f"Cannot place buy order. No valid grid level crossed or grid level is not ready to buy.")
         except ValueError as e:
             self.logger.error(f"Error executing buy order: {e}")
             return balance, crypto_balance
 
-    def execute_sell(self, current_price, previous_price, timestamp, balance, crypto_balance):
-        grid_level_crossed = self.detect_grid_level_crossing(current_price, previous_price, sell=True)
+    def is_valid_buy_level(self, grid_level_crossed):
+        return grid_level_crossed is not None and grid_level_crossed.can_place_buy_order()
 
-        if grid_level_crossed is not None:
-            try:
-                buy_grid_level = self.find_lowest_completed_buy_order_grid_level()
-                buy_order = buy_grid_level.buy_orders[-1] # Get the latest buy_order
-                quantity = buy_order.quantity
-                self.check_sufficient_crypto(crypto_balance, quantity, grid_level_crossed.price)
-                balance, crypto_balance = self.place_sell_order(grid_level_crossed, current_price, quantity, timestamp, balance, crypto_balance)
-                self.reset_grid_cycle(buy_grid_level)
+    def place_buy_order(self, grid_level, current_price, timestamp, balance, crypto_balance):
+        quantity = self.trade_percentage * balance / current_price
+        try:
+            grid_level.place_buy_order(Order(current_price, quantity, OrderType.BUY, timestamp))
+        except ValueError as e:
+            self.logger.error(f"Failed to place buy order at grid level {grid_level.price}: {e}")
+            raise
+
+        trade_value = quantity * current_price
+        buy_fee = trade_value * self.trading_fee
+        balance -= trade_value + buy_fee
+        crypto_balance += quantity
+        self.total_trading_fees += buy_fee
+        return balance, crypto_balance
+
+    def execute_sell(self, current_price, previous_price, timestamp, balance, crypto_balance):
+        try:
+            grid_level_crossed = self.detect_grid_level_crossing(current_price, previous_price, sell=True)
+            
+            if grid_level_crossed is None:
+                self.logger.info("No grid level crossed for selling.")
                 return balance, crypto_balance
-            except ValueError as e:
-                self.logger.warning(f"Failed to place sell order: {e}")
+            
+            buy_grid_level = self.find_lowest_completed_buy_order_grid_level()
+            if not buy_grid_level.buy_orders:
+                self.logger.warning(f"No completed buy orders found for grid level {grid_level_crossed.price}")
+                return balance, crypto_balance
+            
+            buy_order = buy_grid_level.buy_orders[-1] # Get the last buy order
+            quantity = buy_order.quantity
+            self.check_sufficient_crypto(crypto_balance, quantity, grid_level_crossed.price)
+            return self.process_sell_order(grid_level_crossed, current_price, quantity, timestamp, balance, crypto_balance, buy_grid_level)
+
+        except ValueError as e:
+            self.logger.warning(f"Failed to place sell order: {e}")
+            return balance, crypto_balance
+    
+    def process_sell_order(self, grid_level_crossed, current_price, quantity, timestamp, balance, crypto_balance, buy_grid_level):
+        balance, crypto_balance = self.place_sell_order(grid_level_crossed, current_price, quantity, timestamp, balance, crypto_balance)
+        self.reset_grid_cycle(buy_grid_level)
         return balance, crypto_balance
     
     def find_lowest_completed_buy_order_grid_level(self):
@@ -103,14 +113,12 @@ class OrderManager:
 
     def place_sell_order(self, grid_level, current_price, quantity, timestamp, balance, crypto_balance):
         try:
-            sell_order = Order(current_price, quantity, OrderType.SELL, timestamp)
-            grid_level.place_sell_order(sell_order)
+            grid_level.place_sell_order(Order(current_price, quantity, OrderType.SELL, timestamp))
             trade_value = quantity * current_price
             sell_fee = trade_value * self.trading_fee
             balance += trade_value - sell_fee
             self.total_trading_fees += sell_fee
             crypto_balance -= quantity
-            self.sell_orders.append(sell_order)
             self.logger.info(f"Sell order placed at {current_price} on {timestamp}. Updated balance: {balance}, crypto balance: {crypto_balance}")
         except ValueError as e:
             self.logger.error(f"Failed to place sell order at {current_price} on {timestamp}. Error: {e}")
@@ -123,17 +131,15 @@ class OrderManager:
 
     def display_orders(self):
         orders = []
-        
         for grid_level in self.grid_levels.values():
             for buy_order in grid_level.buy_orders:
-                orders.append(self.make_tabulate_order(buy_order, grid_level))
-            
+                orders.append(self.format_order(buy_order, grid_level))
             for sell_order in grid_level.sell_orders:
-                orders.append(self.make_tabulate_order(sell_order, grid_level))
+                orders.append(self.format_order(sell_order, grid_level))
         
         orders.sort(key=lambda x: x[3])  # x[3] is the timestamp
         print(tabulate(orders, headers=["Order Type", "Price", "Quantity", "Timestamp", "Grid Level"]))
     
-    def make_tabulate_order(self, order: Order, grid_level: GridLevel):
+    def format_order(self, order: Order, grid_level: GridLevel):
         order_type = "BUY" if order.order_type == OrderType.BUY else "SELL"
         return [order_type, order.price, order.quantity, order.timestamp, grid_level.price]
--- strategies/grid.py
+++ strategies/grid.py
@@ -4,6 +4,7 @@ from .plotter import Plotter
 from .base import TradingStrategy
 from .grid_manager import GridManager
 from order_management.order_manager import OrderManager
+from order_management.order import Order, OrderType
 from .performance_metrics import PerformanceMetrics
 
 class GridTradingStrategy(TradingStrategy):
@@ -32,42 +33,38 @@ class GridTradingStrategy(TradingStrategy):
         self.grids = self.grid_manager.calculate_grids()
         self.central_price = self.grid_manager.get_central_price()
         self.order_manager.initialize_grid_levels(self.grids, self.central_price)
+        self.start_crypto_balance = self.crypto_balance
 
     def simulate(self):
         self.logger.info("Start trading simulation")
-        close_prices = self.data['close'].values
-        timestamps = self.data.index
-        initial_price = close_prices[0]
-        self.start_crypto_balance = self.crypto_balance
+        self.extract_price_and_timestamps()
     
-        for (current_price, previous_price), current_timestamp in zip(itertools.pairwise(close_prices), timestamps[1:]):
+        for (current_price, previous_price), current_timestamp in zip(itertools.pairwise(self.close_prices), self.timestamps[1:]):
             if self.check_take_profit_stop_loss(current_price):
                 self.logger.info("Take profit or stop loss triggered, ending simulation")
                 break
             self.execute_orders(current_price, previous_price, current_timestamp)
-
-        final_price = close_prices[-1]
-        self.performance_metrics.calculate_gains(initial_price, final_price, self.start_crypto_balance, self.balance, self.crypto_balance)
-        self.data['account_value'] = self.balance + self.crypto_balance * close_prices
-
-    def execute_orders(self, current_price, previous_price, current_timestamp):
-        try:
-            self.attempt_buy_order(current_price, previous_price, current_timestamp)
-        except ValueError as e:
-            self.logger.error(f"Error placing buy order: {e}")
-
-        try:
-            self.attempt_sell_order(current_price, previous_price, current_timestamp)
-        except ValueError as e:
-            self.logger.error(f"Error placing sell order: {e}")
+        self.finalize_simulation()
     
-    def attempt_buy_order(self, current_price, previous_price, timestamp):
-        if self.order_manager.can_execute_buy_order(current_price, timestamp):
-            self.balance, self.crypto_balance = self.order_manager.execute_buy(current_price, previous_price, timestamp, self.balance, self.crypto_balance)
+    def extract_price_and_timestamps(self):
+        self.close_prices = self.data['close'].values
+        self.timestamps = self.data.index
+        self.initial_price = self.close_prices[0]
+    
+    def execute_orders(self, current_price, previous_price, current_timestamp):
+        self.handle_order_execution(current_price, previous_price, current_timestamp, OrderType.BUY)
+        self.handle_order_execution(current_price, previous_price, current_timestamp, OrderType.SELL)
+    
+    def handle_order_execution(self, current_price, previous_price, timestamp, order_type: OrderType):
+        try:
+            self.balance, self.crypto_balance = self.order_manager.execute_order(order_type, current_price, previous_price, timestamp, self.balance, self.crypto_balance)
+        except ValueError as e:
+            self.logger.error(f"Error placing {order_type} order: {e}")
 
-    def attempt_sell_order(self, current_price, previous_price, timestamp):
-        if self.order_manager.can_execute_sell_order(current_price, timestamp):
-            self.balance, self.crypto_balance = self.order_manager.execute_sell(current_price, previous_price, timestamp, self.balance, self.crypto_balance)
+    def finalize_simulation(self):
+        final_price = self.close_prices[-1]
+        self.performance_metrics.calculate_gains(self.initial_price, final_price, self.start_crypto_balance, self.balance, self.crypto_balance)
+        self.data['account_value'] = self.balance + self.crypto_balance * self.close_prices
 
     def calculate_performance_metrics(self):
         final_balance = self.balance + self.crypto_balance * self.data['close'].iloc[-1]
@@ -78,10 +75,16 @@ class GridTradingStrategy(TradingStrategy):
         time_in_loss = round(self.calculate_time_in_profit_loss()[1], 2)
         sharpe_ratio = self.calculate_sharpe_ratio()
         sortino_ratio = self.calculate_sortino_ratio()
-        num_buy_trades = len(self.order_manager.buy_orders)
-        num_sell_trades = len(self.order_manager.sell_orders)
-        performance_summary = self.performance_metrics.generate_performance_summary(self.data, final_balance, self.data['close'].iloc[-1], roi, max_drawdown, max_runup, time_in_profit, time_in_loss, num_buy_trades, num_sell_trades, sharpe_ratio, sortino_ratio)
+        total_buy_trades = 0
+        total_sell_trades = 0
+
+        for grid_level in self.order_manager.grid_levels.values():
+            total_buy_trades += len(grid_level.buy_orders)
+            total_sell_trades += len(grid_level.sell_orders)
+
+        performance_summary = self.performance_metrics.generate_performance_summary(self.data, final_balance, self.data['close'].iloc[-1], roi, max_drawdown, max_runup, time_in_profit, time_in_loss, total_buy_trades, total_sell_trades, sharpe_ratio, sortino_ratio)
         return performance_summary
 
     def plot_results(self):
-        self.plotter.plot_results(self.data, self.grids, self.order_manager.buy_orders, self.order_manager.sell_orders)
+        print("USE grid levels to get buy and sell orders ? -> inject order_manager in Plotter class directly ?")
+        # self.plotter.plot_results(self.data, self.grids, self.order_manager.buy_orders, self.order_manager.sell_orders)

f7f4e840f6b2e5ccf3fcda41cb0a5014fe0e55fd
Jordan TETE <tetej171@gmail.com>
2024-12-06 16:49:46 +0100

    Enhance plotting figure to show volume and candlesticks instead of only close prices and orders

--- strategies/plotter.py
+++ strategies/plotter.py
@@ -1,3 +1,4 @@
+from plotly.subplots import make_subplots
 from typing import List
 import plotly.graph_objects as go
 import pandas as pd
@@ -6,40 +7,46 @@ from core.order_handling.order import Order, OrderSide
 from core.order_handling.order_book import OrderBook
 
 class Plotter:
-    def __init__(
-        self, 
-        grid_manager: GridManager,
-        order_book: OrderBook
-    ):
+    def __init__(self, grid_manager: GridManager, order_book: OrderBook):
         self.grid_manager = grid_manager
         self.order_book = order_book
 
-    def plot_results(
-        self,
-        data: pd.DataFrame
-    ) -> None:
-        fig = self._create_base_figure(data)
-        grids, central_price = self.grid_manager.price_grids, self.grid_manager.central_price
-        # self._add_volume_trace(fig, data)
-        self._add_grid_lines(fig, grids, central_price)
+    def plot_results(self, data: pd.DataFrame) -> None:
+        fig = make_subplots(
+            rows=2,
+            cols=1,
+            shared_xaxes=True,
+            row_heights=[0.85, 0.15],
+            vertical_spacing=0.03
+        )
+
+        self._add_candlestick_trace(fig, data)
+        self._add_grid_lines(fig, self.grid_manager.price_grids, self.grid_manager.central_price)
         self._add_trade_markers(fig, self.order_book.get_completed_orders())
+        self._add_volume_trace(fig, data)
+
         fig.update_layout(
             title="Grid Trading Strategy Results",
-            xaxis_title="Time",
             yaxis_title="Price (USDT)",
-            yaxis2=dict(title="Volume", overlaying="y", side="right", showgrid=False),
+            yaxis2_title="Volume",
+            xaxis=dict(rangeslider=dict(visible=False)),
             showlegend=False,
         )
         fig.show()
 
-    def _create_base_figure(
-        self, 
-        data: pd.DataFrame
-    ) -> go.Figure:
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(x=data.index, y=data['close'], mode='lines', name='Close Price', showlegend=False))
-        return fig
-
+    def _add_candlestick_trace(self, fig: go.Figure, data: pd.DataFrame) -> None:
+        fig.add_trace(
+            go.Candlestick(
+                x=data.index,
+                open=data['open'],
+                high=data['high'],
+                low=data['low'],
+                close=data['close'],
+                name="Candlesticks",
+            ),
+            row=1, col=1
+        )
+    
     def _add_grid_lines(
         self, 
         fig: go.Figure, 
@@ -56,20 +63,40 @@ class Plotter:
                 showlegend=False
             ))
 
-    def _add_trade_markers(
-        self, 
-        fig: go.Figure, 
-        orders: List[Order]
-    ) -> None:
+    def _add_trade_markers(self, fig: go.Figure, orders: List[Order]) -> None:
         for order in orders:
             icon_name = 'triangle-up' if order.side == OrderSide.BUY else 'triangle-down'
             icon_color = 'green' if order.side == OrderSide.BUY else 'red'
-            fig.add_trace(go.Scatter(
-                x=[order.format_last_trade_timestamp()],
-                y=[order.price],
-                mode='markers',
-                marker=dict(symbol=icon_name, color=icon_color, size=10),
-                name=f'{order.side.name} Order',
-                text=f"Price: {order.price}\nQty: {order.filled}\nDate: {order.format_last_trade_timestamp()}",
-                hoverinfo='text'
-            ))
+            fig.add_trace(
+                go.Scatter(
+                    x=[order.format_last_trade_timestamp()],
+                    y=[order.price],
+                    mode='markers',
+                    marker=dict(symbol=icon_name, color=icon_color, size=10),
+                    name=f'{order.side.name} Order',
+                    text=f"Price: {order.price}\nQty: {order.filled}\nDate: {order.format_last_trade_timestamp()}",
+                    hoverinfo='x+y+text',
+                ),
+                row=1, 
+                col=1
+            )
+
+    def _add_volume_trace(self, fig: go.Figure, data: pd.DataFrame) -> None:
+        volume_colors = ['green' if close >= open_ else 'red' for close, open_ in zip(data['close'], data['open'])]
+        
+        fig.add_trace(
+            go.Bar(
+                x=data.index,
+                y=data['volume'],
+                name="Volume",
+                marker=dict(color=volume_colors),
+                opacity=0.7,
+            ),
+            row=2, col=1
+        )
+        
+        fig.update_yaxes(
+            title="Volume",
+            row=2, col=1,
+            gridcolor="lightgray",
+        )
